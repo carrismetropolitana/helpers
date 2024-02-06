9159233e46933ab1fa5bfdd98383b25760a49855
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-02-06 13:48:45 +0000

    Streaming transactions

---
+++ intermodal-parser/stop_attributes.js
@@ -0,0 +1,81 @@
+/* * */
+
+const fs = require('fs');
+const Papa = require('papaparse');
+
+/* * */
+
+const formatStops = async () => {
+  //
+
+  //
+  // 0. Get latest data from Intermodal
+
+  console.log('• Fetching Intermodal for latest updates...');
+
+  const rawData = await fetch('https://api.intermodal.pt/v1/regions/1/stops/full').then((res) => res.json());
+
+  //
+  // 1. Format the raw data from Intermodal
+
+  const result = [];
+
+  console.log('• Preparing ' + rawData.length + ' stops...');
+
+  for (const stop of rawData) {
+    //
+
+    // Check if this stop is for Carris Metropolitana
+    const stopOperator = stop.operators.find((item) => item.operator_id === 1);
+    if (!stopOperator) continue;
+
+    if (stopOperator.name.substring(0, 2) !== '02') continue;
+
+    // Create file
+    result.push({
+      stop_id: stopOperator.name,
+      stop_lat: stop.lat.toFixed(6),
+      stop_lon: stop.lon.toFixed(6),
+      stop_name: stopOperator.stop_ref,
+      has_shelter: stop.has_shelter ? 1 : 0,
+      imd_id: stop.id,
+    });
+  }
+
+  //
+  // 2. Save the formatted data into a CSV file
+
+  console.log('• Saving data to CSV file.');
+
+  const csvFilename = 'intermodal-has_shelter.csv';
+  const csvData = Papa.unparse(result, { header: true });
+  fs.writeFile(csvFilename, csvData, 'utf8', (err) => {
+    if (err) {
+      console.log('! An error occured while writing data to CSV file.');
+      return console.log(err);
+    }
+    console.log('• CSV file has been created.');
+  });
+
+  console.log('• Done! Kept ' + result.length + ' stops and created 1 file.');
+};
+
+/* * *
+ * ONE TIME EXECUTION
+ */
+(async () => {
+  console.log();
+  console.log('* * * * * * * * * * * * * * * * * * * * * * * * * *');
+  console.log('> PARSER');
+  const start = new Date();
+  console.log('> Parsing started on ' + start.toISOString());
+
+  /* * * * * * * * * * * * */
+  /* */ await formatStops();
+  /* * * * * * * * * * * * */
+
+  const syncDuration = new Date() - start;
+  console.log('> Operation took ' + syncDuration / 1000 + ' seconds.');
+  console.log('* * * * * * * * * * * * * * * * * * * * * * * * * *');
+  console.log();
+})();
---
+++ validations-checker/index.js
@@ -0,0 +1,149 @@
+/* * */
+
+const fs = require('fs');
+require('dotenv').config();
+const REALTIMEDB = require('./REALTIMEDB');
+const Papa = require('papaparse');
+/* * */
+
+/* * *
+ * ONE TIME EXECUTION
+ */
+(async () => {
+  console.log();
+  console.log('* * * * * * * * * * * * * * * * * * * * * * * * * *');
+  const start = new Date();
+  console.log('> Started on ' + start.toISOString());
+
+  /* * * * * * * * * * * * */
+
+  await REALTIMEDB.connect();
+
+  /* * */
+
+  const simple = { _id: '2a7264ab-c91b-4ce8-be1e-b000ebf0d3f1' };
+
+  const tarifasDeBordo = {
+    'transaction.transactionDate': {
+      $gte: '2024-01-01T04:00:00',
+      $lte: '2024-02-01T03:59:59',
+    },
+    operator: '43',
+    'transaction.productLongID': {
+      $in: ['id-prod-tarifa-rapida', 'id-prod-tarifa-local', 'id-prod-tarifa-urbano', 'id-prod-tarifa-praia', 'id-prod-tarifa-local-bon-3223-43'],
+    },
+  };
+
+  const preComprado = {
+    'transaction.transactionDate': {
+      $gte: '2024-01-01T04:00:00',
+      $lte: '2024-02-01T03:59:59',
+    },
+    // operator: '43',
+    'transaction.productLongID': 'id-prod-zapping',
+  };
+
+  const apex311 = {
+    operator: '41',
+    'transaction.transactionDate': {
+      $gte: '2024-01-25T04:00:00',
+      //   $lte: '2024-02-01T03:59:59',
+    },
+    // 'transaction.apexVersion': '3.1.1',
+  };
+
+  /* * */
+
+  let isFirstDoc = true;
+  let counter = 0;
+
+  const resultStream = REALTIMEDB.ValidationEntity.find(apex311, { allowDiskUse: true, maxTimeMS: 180000 }).stream(); // .pipe(jsonToCsv()).pipe(fs.createWriteStream('./data.csv'));
+
+  resultStream.on('error', function (err) {
+    console.error(err);
+  });
+
+  resultStream.on('data', function (doc) {
+    console.log(counter);
+    counter++;
+    console.log('apexVersion', doc.transaction.apexVersion, 'apexTransactionVersion', doc.transaction.apexTransactionVersion);
+    return;
+    if (!doc.transaction.apexVersion.startsWith('3')) return;
+    const papaparseOptions = { quotes: true, header: true };
+    const csvData = Papa.unparse(
+      [
+        {
+          _id: doc._id,
+          'transaction.validationType': doc.transaction.validationType,
+          'transaction.productLongID': doc.transaction.productLongID,
+          'transaction.tickLoadDate': doc.transaction.tickLoadDate,
+          'transaction.tickLoadMachCode': doc.transaction.tickLoadMachCode,
+          'transaction.tickLoadNumbDaily': doc.transaction.tickLoadNumbDaily,
+          'transaction.unitsRemaining': doc.transaction.unitsRemaining,
+          'transaction.contractNumber': doc.transaction.contractNumber,
+          'transaction.eventType': doc.transaction.eventType,
+          'transaction.validationStatus': doc.transaction.validationStatus,
+          'transaction.whiteListItemID': doc.transaction.whiteListItemID,
+          'transaction.calendarID': doc.transaction.calendarID,
+          'transaction.spatialValidityLongID': doc.transaction.spatialValidityLongID,
+          'transaction.productMatrixElementID': doc.transaction.productMatrixElementID,
+          'transaction.validationDataLongID': doc.transaction.validationDataLongID,
+          'transaction.profilesUsedCount': doc.transaction.profilesUsedCount,
+          'transaction.profilesUsedData': doc.transaction.profilesUsedData,
+          'transaction.greylistItemCount': doc.transaction.greylistItemCount,
+          'transaction.greyListItemsData': doc.transaction.greyListItemsData,
+          'transaction.validatorID': doc.transaction.validatorID,
+          'transaction.stopLongID': doc.transaction.stopLongID,
+          'transaction.operationPlanID': doc.transaction.operationPlanID,
+          'transaction.vehicleID': doc.transaction.vehicleID,
+          'transaction.zoneLongID': doc.transaction.zoneLongID,
+          'transaction.cardTypeID': doc.transaction.cardTypeID,
+          'transaction.cardPhysicalType': doc.transaction.cardPhysicalType,
+          'transaction.cardNetworkID': doc.transaction.cardNetworkID,
+          'transaction.cardIssuer': doc.transaction.cardIssuer,
+          //   'transaction.cardNumber.$numberLong': doc.transaction.cardNumber.$numberLong,
+          'transaction.cardSerialNumber': doc.transaction.cardSerialNumber,
+          'transaction.operatorLongID': doc.transaction.operatorLongID,
+          'transaction.networkID': doc.transaction.networkID,
+          'transaction.channelID': doc.transaction.channelID,
+          'transaction.deviceID': doc.transaction.deviceID,
+          'transaction.apexVersion': doc.transaction.apexVersion,
+          'transaction.vivaVersion': doc.transaction.vivaVersion,
+          'transaction.technicalParametersVersion': doc.transaction.technicalParametersVersion,
+          'transaction.commercialOfferVersion': doc.transaction.commercialOfferVersion,
+          'transaction.networkVersion': doc.transaction.networkVersion,
+          'transaction.actionListsVersion': doc.transaction.actionListsVersion,
+          'transaction.apexTransactionType': doc.transaction.apexTransactionType,
+          'transaction.apexTransactionVersion': doc.transaction.apexTransactionVersion,
+          'transaction.transactionId': doc.transaction.transactionId,
+          'transaction.transactionGroupId': doc.transaction.transactionGroupId,
+          'transaction.transactionDate': doc.transaction.transactionDate,
+          'transaction.signedData': doc.transaction.signedData,
+          'transaction.mac': doc.transaction.mac,
+          createdAt: doc.createdAt,
+          batchId: doc.batchId,
+          operator: doc.operator,
+          siitState: doc.siitState,
+          csvTransaction: doc.csvTransaction,
+          errorList: doc.errorList.join('|'),
+          _class: doc._class,
+        },
+      ],
+      papaparseOptions
+    );
+    fs.appendFileSync(`result.csv`, csvData);
+    if (isFirstDoc) isFirstDoc = false;
+  });
+
+  resultStream.on('end', function () {
+    REALTIMEDB.client.close();
+    console.log('closing connection');
+  });
+
+  /* * * * * * * * * * * * */
+
+  const syncDuration = new Date() - start;
+  console.log('> Operation took ' + syncDuration / 1000 + ' seconds.');
+  console.log('* * * * * * * * * * * * * * * * * * * * * * * * * *');
+  console.log();
+})();
