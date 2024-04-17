c7a25e83b7d39f8731c6a4d339349a5a0ae9aff1
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-04-17 19:44:20 +0100

    Update has_shelter.js

--- intermodal-parser/has_shelter.js
+++ intermodal-parser/has_shelter.js
@@ -39,13 +39,75 @@ const formatStops = async () => {
 
     const thisStopData = allStopsData.find((item) => item.id === stopOperator.stop_ref);
 
+    console.log(stop);
+
+    const iuhds = {
+      id: 10480,
+      name: 'Igreja do Murtal',
+      short_name: null,
+      locality: null,
+      street: null,
+      door: null,
+      parish: 3,
+      lat: 38.702896,
+      lon: -9.3618603,
+      notes: null,
+      tags: [],
+      schedules: null,
+      flags: null,
+      has_sidewalk: null,
+      has_sidewalked_path: null,
+      has_shelter: null,
+      has_cover: null,
+      has_bench: null,
+      has_trash_can: null,
+      has_waiting_times: null,
+      has_ticket_seller: null,
+      has_costumer_support: null,
+      advertisement_qty: null,
+      has_crossing: null,
+      has_wide_access: null,
+      has_flat_access: null,
+      has_tactile_access: null,
+      illumination_strength: null,
+      illumination_position: null,
+      has_illuminated_path: null,
+      has_visibility_from_within: null,
+      has_visibility_from_area: null,
+      is_visible_from_outside: null,
+      parking_visibility_impairment: null,
+      parking_local_access_impairment: null,
+      parking_area_access_impairment: null,
+      tmp_issues: [],
+      verification_level: 192,
+      service_check_date: null,
+      infrastructure_check_date: null,
+      updater: 1,
+      osm_id: 8140011563,
+      operators: [
+        {
+          operator_id: 1,
+          stop_ref: '050478',
+          name: 'Av Maria Lamas - Igreja',
+          source: 'osm',
+        },
+      ],
+      verified_position: true,
+      update_date: '2022-11-21T23:18:46.674962Z',
+    };
+
     // Create file
     result.push({
       stop_id: stopOperator.stop_ref,
       stop_lat: stop.lat.toFixed(6),
       stop_lon: stop.lon.toFixed(6),
       stop_name: stopOperator.name,
+
       has_shelter: stop.has_shelter,
+      has_shelter: stop.has_shelter,
+      has_shelter: stop.has_shelter,
+      has_shelter: stop.has_shelter,
+
       imd_id: stop.id,
       lines: thisStopData?.lines?.join('|'),
     });
