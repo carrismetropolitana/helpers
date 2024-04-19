ebc90d94860ba490d03ffc50a7fd126c93af99af
João de Vasconcelos <johnyvasconcelos@icloud.com>
2024-04-19 17:45:04 +0100

    Cleanup

---
+++ validations-checker/events_multiple.js
@@ -0,0 +1,47 @@
+/* * */
+
+const fs = require('fs');
+require('dotenv').config();
+const REALTIMEDB = require('./REALTIMEDB');
+
+/* * */
+
+(async () => {
+    console.log();
+    console.log('* * * * * * * * * * * * * * * * * * * * * * * * * *');
+    const start = new Date();
+    console.log('> Started on ' + start.toISOString());
+
+    /* * * * * * * * * * * * */
+
+    await REALTIMEDB.connect();
+
+    /* * */
+
+    const startDateMillis = 1713524400000;
+    const endDateMillis = 1713538800000;
+
+    /* * */
+
+    const query = {
+        millis: { $gte: startDateMillis, $lte: endDateMillis },
+        'content.entity.vehicle.agencyId': '43',
+        'content.entity.vehicle.vehicle._id': '2239',
+        // 'content.entity.vehicle.trip.tripId': '3507_0_1_1330_1359_0_ESC_DU',
+    };
+
+    /* * */
+
+    console.log('Searching events');
+    console.log(query);
+
+    const result = await REALTIMEDB.VehicleEvents.find(query, { allowDiskUse: true, maxTimeMS: 180000 }).toArray();
+
+    console.log(result);
+
+    const resultString = JSON.stringify(result, null, 2);
+
+    fs.writeFileSync(`events-43-${startDateMillis}-${endDateMillis}.json`, resultString);
+
+    //
+})();
