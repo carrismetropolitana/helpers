805f581c229897403e6bfd1784cc328a713eb0b7
João de Vasconcelos <contact@joao.earth>
2025-01-22 17:38:08 +0000

    Refactor geojson feature parsing and add handling for skipped localities

--- geojson-transform/index.js
+++ geojson-transform/index.js
@@ -10,78 +10,99 @@ import yazl from 'yazl';
 const formatFeatures = async () => {
 	//
 
-	// console.log('• Parsing districts...');
+	console.log('• Parsing districts...');
 
-	// const districtsText = fs.readFileSync('districts.json', { encoding: 'utf8' });
-	// const districtsData = JSON.parse(districtsText);
+	const districtsText = fs.readFileSync('districts.json', { encoding: 'utf8' });
+	const districtsData = JSON.parse(districtsText);
 
-	// for (const districtFeature of districtsData.features) {
-	// 	//
+	for (const districtFeature of districtsData.features) {
+		//
 
-	// 	districtFeature.id = districtFeature.properties.id;
+		districtFeature.id = districtFeature.properties.id;
 
-	// 	districtFeature.properties['area_ha'] = districtFeature.properties['Area_ha'];
+		districtFeature.properties['area_ha'] = districtFeature.properties['Area_ha'];
 
-	// 	delete districtFeature.properties['Area_ha'];
-	// 	delete districtFeature.properties['Perim_km'];
-	// 	delete districtFeature.properties['Shape_Length'];
-	// 	delete districtFeature.properties['Shape_Area'];
-	// 	delete districtFeature.properties['OBJECTID'];
+		delete districtFeature.properties['Area_ha'];
+		delete districtFeature.properties['Perim_km'];
+		delete districtFeature.properties['Shape_Length'];
+		delete districtFeature.properties['Shape_Area'];
+		delete districtFeature.properties['OBJECTID'];
 
-	// 	//
-	// }
+		//
+	}
 
-	// fs.writeFileSync(`districts_parsed.json`, JSON.stringify(districtsData));
+	fs.writeFileSync(`districts_parsed.json`, JSON.stringify(districtsData));
 
-	/* * */
-
-	// console.log('• Parsing municipalities...');
-
-	// const municipalitiesText = fs.readFileSync('municipalities.json', { encoding: 'utf8' });
-	// const municipalitiesData = JSON.parse(municipalitiesText);
-
-	// for (const municipalityFeature of municipalitiesData.features) {
-	// 	//
-
-	// 	municipalityFeature.id = municipalityFeature.properties.id;
-
-	// 	municipalityFeature.properties['area_ha'] = municipalityFeature.properties['Area_ha'];
-
-	// 	delete municipalityFeature.properties['Area_ha'];
-	// 	delete municipalityFeature.properties['Perim_km'];
-	// 	delete municipalityFeature.properties['Shape_Length'];
-	// 	delete municipalityFeature.properties['Shape_Area'];
-	// 	delete municipalityFeature.properties['OBJECTID'];
-
-	// 	//
-	// }
-
-	// fs.writeFileSync(`municipalities_parsed.json`, JSON.stringify(municipalitiesData));
+	const districtsZip = new yazl.ZipFile();
+	await new Promise((resolve) => {
+		districtsZip.addFile(path.resolve(`./districts_parsed.json`), 'districts.json', { compressionLevel: 9 });
+		districtsZip.outputStream.pipe(fs.createWriteStream(path.resolve(`./districts.zip`))).on('close', () => resolve());
+		districtsZip.end();
+	});
 
 	/* * */
 
-	// console.log('• Parsing parishes...');
+	console.log('• Parsing municipalities...');
 
-	// const parishesText = fs.readFileSync('parishes.json', { encoding: 'utf8' });
-	// const parishesData = JSON.parse(parishesText);
+	const municipalitiesText = fs.readFileSync('municipalities.json', { encoding: 'utf8' });
+	const municipalitiesData = JSON.parse(municipalitiesText);
 
-	// for (const parishFeature of parishesData.features) {
-	// 	//
+	for (const municipalityFeature of municipalitiesData.features) {
+		//
 
-	// 	parishFeature.id = parishFeature.properties.id;
+		municipalityFeature.id = municipalityFeature.properties.id;
 
-	// 	parishFeature.properties['area_ha'] = parishFeature.properties['Area_ha'];
+		municipalityFeature.properties['area_ha'] = municipalityFeature.properties['Area_ha'];
 
-	// 	delete parishFeature.properties['Area_ha'];
-	// 	delete parishFeature.properties['Perim_km'];
-	// 	delete parishFeature.properties['Shape_Length'];
-	// 	delete parishFeature.properties['Shape_Area'];
-	// 	delete parishFeature.properties['OBJECTID'];
+		delete municipalityFeature.properties['Area_ha'];
+		delete municipalityFeature.properties['Perim_km'];
+		delete municipalityFeature.properties['Shape_Length'];
+		delete municipalityFeature.properties['Shape_Area'];
+		delete municipalityFeature.properties['OBJECTID'];
 
-	// 	//
-	// }
+		//
+	}
 
-	// fs.writeFileSync(`parishes_parsed.json`, JSON.stringify(parishesData));
+	fs.writeFileSync(`municipalities_parsed.json`, JSON.stringify(municipalitiesData));
+
+	const municipalitiesZip = new yazl.ZipFile();
+	await new Promise((resolve) => {
+		municipalitiesZip.addFile(path.resolve(`./municipalities_parsed.json`), 'municipalities.json', { compressionLevel: 9 });
+		municipalitiesZip.outputStream.pipe(fs.createWriteStream(path.resolve(`./municipalities.zip`))).on('close', () => resolve());
+		municipalitiesZip.end();
+	});
+
+	/* * */
+
+	console.log('• Parsing parishes...');
+
+	const parishesText = fs.readFileSync('parishes.json', { encoding: 'utf8' });
+	const parishesData = JSON.parse(parishesText);
+
+	for (const parishFeature of parishesData.features) {
+		//
+
+		parishFeature.id = parishFeature.properties.id;
+
+		parishFeature.properties['area_ha'] = parishFeature.properties['Area_ha'];
+
+		delete parishFeature.properties['Area_ha'];
+		delete parishFeature.properties['Perim_km'];
+		delete parishFeature.properties['Shape_Length'];
+		delete parishFeature.properties['Shape_Area'];
+		delete parishFeature.properties['OBJECTID'];
+
+		//
+	}
+
+	fs.writeFileSync(`parishes_parsed.json`, JSON.stringify(parishesData));
+
+	const parishesZip = new yazl.ZipFile();
+	await new Promise((resolve) => {
+		parishesZip.addFile(path.resolve(`./parishes_parsed.json`), 'parishes.json', { compressionLevel: 9 });
+		parishesZip.outputStream.pipe(fs.createWriteStream(path.resolve(`./parishes.zip`))).on('close', () => resolve());
+		parishesZip.end();
+	});
 
 	/* * */
 
@@ -93,6 +114,13 @@ const formatFeatures = async () => {
 	for (const localityFeature of localitiesData.features) {
 		//
 		try {
+			//
+
+			if (localityFeature.properties['LUG11'] === '999999') {
+				console.log('skipped 999999');
+				continue;
+			}
+
 			localityFeature.id = localityFeature.properties['LUG11'];
 			localityFeature.properties['id'] = localityFeature.properties['LUG11'];
 
@@ -117,16 +145,13 @@ const formatFeatures = async () => {
 		//
 	}
 
-	console.log(localitiesData.features[0]);
-
 	fs.writeFileSync(`localities_parsed.json`, JSON.stringify(localitiesData));
 
-	const outputZip = new yazl.ZipFile();
-
+	const localitiesZip = new yazl.ZipFile();
 	await new Promise((resolve) => {
-		outputZip.addFile(path.resolve(`./localities_parsed.json`), 'localities.json', { compressionLevel: 9 });
-		outputZip.outputStream.pipe(fs.createWriteStream(path.resolve(`./localities.zip`))).on('close', () => resolve());
-		outputZip.end();
+		localitiesZip.addFile(path.resolve(`./localities_parsed.json`), 'localities.json', { compressionLevel: 9 });
+		localitiesZip.outputStream.pipe(fs.createWriteStream(path.resolve(`./localities.zip`))).on('close', () => resolve());
+		localitiesZip.end();
 	});
 
 	//
