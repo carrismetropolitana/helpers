59d0c382a66fee051d79e62dab10a9dd311daca0
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-03-31 18:11:49 +0100

    Initial commit, basic generation started
    Not finished, still has some bugs

---
+++ persona_generator/structure.js
@@ -0,0 +1,35 @@
+const categories = [
+
+    { 
+        _id: "base",
+        items: [
+            {
+                _id: "base-1",
+                images: [
+                    { filename: "base.png", order: 1 },
+                ],
+            }
+        ]
+    },
+
+    { 
+        _id: "hair",
+        items: [
+            {
+                _id: "afro",
+                images: [
+                    { filename: "afro-back.png", order: 0 },
+                    { filename: "afro-front.png", order: 2 },
+                ],
+            },
+            {
+                _id: "afro",
+                images: [
+                    { filename: "afro-back.png", order: 0 },
+                    { filename: "afro-front.png", order: 2 },
+                ],
+            }
+        ]
+    }
+
+]
