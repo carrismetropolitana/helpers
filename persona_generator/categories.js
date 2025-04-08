87499aa288ab7214419dac8cd854adc8592836d3
Bruno Castelo <brunoalexcastelo@gmail.com>
2025-04-08 17:19:17 +0100

    Refactor code structure for improved readability and maintainability

--- persona_generator/categories.js
+++ persona_generator/categories.js
@@ -11,190 +11,42 @@ export const categories = [
         ]
     },
     { 
-        _id: "Cabelo",
-        items: [
-            {
-                _id: "afro",
-                images: [
-                    { filename: "12_Afro_Back.png", order: 0 },
-                    { filename: "5_Mini_Afro.png", order: 2 },
-                ],
-            },
-            {
-                _id: "mullet",
-                images: [
-                    { filename: "1_Mullet.png", order: 2 },
-                ],
-            },
-            {
-                _id: "popa",
-                images: [
-                    { filename: "2_Popa.png", order: 2 },
-                ],
-            },
-            {
-                _id: "bob",
-                images: [
-                    { filename: "3_Bob.png", order: 2 },
-                ],
-            },
-            {
-                _id: "crista",
-                images: [
-                    { filename: "4_Crista.png", order: 2 },
-                ],
-            },
-            {
-                _id: "mini-afro",
-                images: [
-                    { filename: "2_Popa.png", order: 2 },
-                ],
-            },
-            {
-                _id: "liso",
-                images: [
-                    { filename: "6_Liso.png", order: 2 },
-                ],
-            },
-            {
-                _id: "careca",
-                images: [
-                    { filename: "7_Careca.png", order: 2 },
-                ],
-            },
-            {
-                _id: "rapado",
-                images: [
-                    { filename: "8_Rapado.png", order: 2 },
-                ],
-            },
-            {
-                _id: "encaraculado",
-                images: [
-                    { filename: "9_Encaraculado.png", order: 2 },
-                ],
-            },
-            {
-                _id: "buzzcut",
-                images: [
-                    { filename: "10_Buzzcut.png", order: 2 },
-                ],
-            },
-            {
-                _id: "daenerys",
-                images: [
-                    { filename: "11_Daenerys.png", order: 2 },
-                ],
-            }
-        ]
-    },
-    { 
-        _id: "Barbas",
-        items: [
-            {
-                _id: "por_fazer",
-                images: [
-                    { filename: "1_Por_fazer.png", order: 3 },
-                ],
-            },
-            {
-                _id: "comprida",
-                images: [
-                    { filename: "2_Comprida.png", order: 3 },
-                ],
-            },
-            {
-                _id: "p_fazer_bigode",
-                images: [
-                    { filename: "3_P_Fazer_Bigode.png", order: 3 },
-                ],
-            },
-            {
-                _id: "pera",
-                images: [
-                    { filename: "4_Pêra.png", order: 3 },
-                ],
-            },
-            {
-                _id: "bigode",
-                images: [
-                    { filename: "5_Bigode.png", order: 3 },
-                ],
-            }
-        ]
-    },
-    { 
-        _id: "Acessórios",
-        items: [
-            {
-                _id: "piercing_nariz",
-                images: [
-                    { filename: "1_Piercing_Nariz.png", order: 4 },
-                ],
-            },
-            {
-                _id: "piercing_septo",
-                images: [
-                    { filename: "2_Piercing_ Septo.png", order: 4 },
-                ],
-            },
-            {
-                _id: "piercing_sobrancelha",
-                images: [
-                    { filename: "3_Piercing_Sobrancelha.png", order: 4 },
-                ],
-            },
-            {
-                _id: "oculos",
-                images: [
-                    { filename: "4_Oculos.png", order: 4 },
-                ],
-            },
-            {
-                _id: "piercing_labio",
-                images: [
-                    { filename: "5_Piercing_Labio.png", order: 4 },
-                ],
-            }
-        ]
-    },
-    { 
         _id: "Orelhas",
         items: [
             {
                 _id: "normal",
                 images: [
-                    { filename: "1_Normal.png", order: 5 },
+                    { filename: "1_Normal.png", order: 2 },
                 ],
             },
             {
                 _id: "c_brinco",
                 images: [
-                    { filename: "2_C_Brinco.png", order: 5 },
+                    { filename: "2_C_Brinco.png", order: 2 },
                 ],
             },
             {
                 _id: "c_argola",
                 images: [
-                    { filename: "3_C_Argola.png", order: 5 },
+                    { filename: "3_C_Argola.png", order: 2 },
                 ],
             },
             {
                 _id: "c_argola_xl",
                 images: [
-                    { filename: "4_C_Argola_XL.png", order: 5 },
+                    { filename: "4_C_Argola_XL.png", order: 2 },
                 ],
             },
             {
                 _id: "alargador_s",
                 images: [
-                    { filename: "5_Alargador_S.png", order: 5 },
+                    { filename: "5_Alargador_S.png", order: 2 },
                 ],
             },
             {
                 _id: "alargador_xl",
                 images: [
-                    { filename: "6_Alargado_XL.png", order: 5 },
+                    { filename: "6_Alargado_XL.png", order: 2 },
                 ],
             }
         ]
@@ -205,13 +57,13 @@ export const categories = [
             {
                 _id: "tattoo_1",
                 images: [
-                    { filename: "Tattoo_1.png", order: 6 },
+                    { filename: "Tattoo_1.png", order: 3 },
                 ],
             },
             {
                 _id: "tattoo_0",
                 images: [
-                    { filename: "Tattoo_0.png", order: 6 },
+                    { filename: "Tattoo_0.png", order: 3 },
                 ],
             }
         ]
@@ -222,39 +74,188 @@ export const categories = [
             {
                 _id: "manga_cava",
                 images: [
-                    { filename: "1_Mangas_Cavas.png", order: 7 },
+                    { filename: "1_Mangas_Cavas.png", order: 4 },
                 ],
             },
             {
                 _id: "perolas",
                 images: [
-                    { filename: "2_Pérolas.png", order: 7 },
+                    { filename: "2_Pérolas.png", order: 4 },
                 ],
             },
             {
                 _id: "camisa_roxa",
                 images: [
-                    { filename: "3_Camisa_Roxa.png", order: 7 },
+                    { filename: "3_Camisa_Roxa.png", order: 4 },
                 ],
             },
             {
                 _id: "camisa_azul",
                 images: [
-                    { filename: "4_Camisa Azul.png", order: 7 },
+                    { filename: "4_Camisa Azul.png", order: 4 },
                 ],
             },
             {
                 _id: "top_azul_mar",
                 images: [
-                    { filename: "5_Top_Azul_Mar.png", order: 7 },
+                    { filename: "5_Top_Azul_Mar.png", order: 4 },
                 ],
             },
             {
                 _id: "sweatshirt_amarela",
                 images: [
-                    { filename: "6_Sweatshirt_Amarela.png", order: 7 },
+                    { filename: "6_Sweatshirt_Amarela.png", order: 4 },
                 ],
             },
+        ]
+    },
+    { 
+        _id: "Cabelo",
+        items: [
+            {
+                _id: "afro",
+                images: [
+                    { filename: "12_Afro_Back.png", order: 0 },
+                    { filename: "5_Mini_Afro.png", order: 5 },
+                ],
+            },
+            {
+                _id: "mullet",
+                images: [
+                    { filename: "1_Mullet.png", order: 5 },
+                ],
+            },
+            {
+                _id: "popa",
+                images: [
+                    { filename: "2_Popa.png", order: 5 },
+                ],
+            },
+            {
+                _id: "bob",
+                images: [
+                    { filename: "3_Bob.png", order: 5 },
+                ],
+            },
+            {
+                _id: "crista",
+                images: [
+                    { filename: "4_Crista.png", order: 5 },
+                ],
+            },
+            {
+                _id: "mini-afro",
+                images: [
+                    { filename: "2_Popa.png", order: 5 },
+                ],
+            },
+            {
+                _id: "liso",
+                images: [
+                    { filename: "6_Liso.png", order: 5 },
+                ],
+            },
+            {
+                _id: "careca",
+                images: [
+                    { filename: "7_Careca.png", order: 5},
+                ],
+            },
+            {
+                _id: "rapado",
+                images: [
+                    { filename: "8_Rapado.png", order: 5 },
+                ],
+            },
+            {
+                _id: "encaraculado",
+                images: [
+                    { filename: "9_Encaraculado.png", order: 5 },
+                ],
+            },
+            {
+                _id: "buzzcut",
+                images: [
+                    { filename: "10_Buzzcut.png", order: 5 },
+                ],
+            },
+            {
+                _id: "daenerys",
+                images: [
+                    { filename: "11_Daenerys.png", order: 5 },
+                ],
+            }
+        ]
+    },
+    
+    { 
+        _id: "Barbas",
+        items: [
+            {
+                _id: "por_fazer",
+                images: [
+                    { filename: "1_Por_fazer.png", order: 6},
+                ],
+            },
+            {
+                _id: "comprida",
+                images: [
+                    { filename: "2_Comprida.png", order: 6 },
+                ],
+            },
+            {
+                _id: "p_fazer_bigode",
+                images: [
+                    { filename: "3_P_Fazer_Bigode.png", order: 6 },
+                ],
+            },
+            {
+                _id: "pera",
+                images: [
+                    { filename: "4_Pêra.png", order: 6 },
+                ],
+            },
+            {
+                _id: "bigode",
+                images: [
+                    { filename: "5_Bigode.png", order: 6 },
+                ],
+            }
+        ]
+    },
+    { 
+        _id: "Acessórios",
+        items: [
+            {
+                _id: "piercing_nariz",
+                images: [
+                    { filename: "1_Piercing_Nariz.png", order: 7 },
+                ],
+            },
+            {
+                _id: "piercing_septo",
+                images: [
+                    { filename: "2_Piercing_ Septo.png", order: 7 },
+                ],
+            },
+            {
+                _id: "piercing_sobrancelha",
+                images: [
+                    { filename: "3_Piercing_Sobrancelha.png", order: 7 },
+                ],
+            },
+            {
+                _id: "oculos",
+                images: [
+                    { filename: "4_Oculos.png", order: 7 },
+                ],
+            },
+            {
+                _id: "piercing_labio",
+                images: [
+                    { filename: "5_Piercing_Labio.png", order: 7 },
+                ],
+            }
         ]
     },
   ];
